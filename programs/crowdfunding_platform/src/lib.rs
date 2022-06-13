25afea08316d18d4a783d7bbd4b6bd8201053acc
crustydev <ademola1145@gmail.com>
2022-06-13 21:16:57 +0100

    Write struct to represent application state

--- programs/crowdfunding_platform/src/lib.rs
+++ programs/crowdfunding_platform/src/lib.rs
@@ -15,4 +15,24 @@ pub mod crowdfunding_platform {
 pub struct Initialize {}
 
 
+#[account]
+#[derive(Default)]
+pub struct CrowdFundState {
+    // The user starting a fundraiser
+    user: Pubkey,
 
+    // The wallet that'll receive the tokens
+    funding_wallet: Pubkey,
+
+    // The fundraiser description, should not take > 200 bytes of storage
+    description: String,
+
+    // The amount of tokens the user is trying to raise
+    target: u64,
+
+    // The current balance of the user's fundraising account
+    balance: u64,
+
+    // The mint of the token the user is trying to raise
+    token_mint: Pubkey,
+}
